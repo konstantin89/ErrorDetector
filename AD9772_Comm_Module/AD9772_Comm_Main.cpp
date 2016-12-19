044a0ca38361b72e808e30a4de284d4506418b58
Kosta <kosta@localhost.localdomain>
2016-12-19 15:53:26 +0200

    AD9772_Comm_Main.cpp changed to check sample reading rate.

--- AD9772_Comm_Module/AD9772_Comm_Main.cpp
+++ AD9772_Comm_Module/AD9772_Comm_Main.cpp
@@ -1,70 +1,183 @@
+/*****************************************************************
+*
+* @author: Kosta Bulgakov
+*
+* @date: 19\12\2016
+*
+* @brief: Minimal code for interfacing with AD7992 that
+*         used to know the maximum sample reading rate.
+*
+*         The code contains long functions that made with 
+*         bad coding style that used only to find the 
+*         maximum possible reading rate of samples from AD7992.
+*
+*****************************************************************/
 
+
+/*********************** Includes *******************************/
 #include <iostream>
 #include "AD9772_Comm.h"
+#include <chrono>
+
+/**************** Defines for AD7992 device  *******************/
+// Flag for debugging.
+#define DEBUG 0
+
+// Print samples to standard output for debugging. 
+#define print_debug(val, chanel)\
+            do{ if(DEBUG) printf("Value:%d, Chanel:%d\n", val, chanel);} while(0)
+
+//Print errror to user.
+#define print_error(errorText)\
+            fprintf(stderr, errorText);
+
 
 #define ADDRESS_AS_GND 0x23
-
-#define CONF_REG_VAL_BIN 0b00111000
 #define CONF_REG_VAL_HEX 0x38
+
+//Do not ommit LSB bits from samples.
+#define RESULT_MASK  0xfff 
+
+
+/*
+* Function that used to run AD7992 device in command
+* mode and read samples from it.
+*
+* The function made to count the sample per second rate
+* of the device when running the minimum needed code.
+* 
+* This test used to chech how the error detector application
+* reading rate is close to the optimal rate that is counted
+* in this program.
+*
+*/
+int commandModeTwoCh();
 
 
 int main()
 {
-	BYTE buff[3];
-	
-	std::cout << "AD9772 main started." << std::endl;
-    AD9772_Comm ad7992Comm(ADDRESS_AS_GND);
-    if(ad7992Comm.openCommunicatioBus() == MY_ERROR)
-	{
-			std::cout << "Open communication failed" << std::endl;
-	}
-	if(ad7992Comm.initCommunication() == MY_ERROR)
-	{
-		std::cout << "Init communication failed" << std::endl;
-	}
-	
+	commandModeTwoCh();
+	return SUCCESS;
+}
 
-	///////////////////////////////////////////////////////////////////////
 
-	
-	if(ad7992Comm.setControlRegister(CONFIGURATION_REG, CONF_REG_VAL_HEX) 
-		                                                      == MY_ERROR)
+int commandModeTwoCh()
+{
+	long sampleCount = 0;
+    BYTE buff[4];
+    int fileDescriptor = 0;
+    UINT writenByteNum = 0;
+
+    /*
+    * Get comminication descriptor to I2C device
+    * from linux.
+    */
+	char I2C_Device_Name[40] = "/dev/i2c-1";
+	fileDescriptor = open(I2C_Device_Name, O_RDWR);
+	if(fileDescriptor < 0)
 	{
-		std::cout << "Setting configutation register failed" << std::endl;
+		print_error("open() failed\n");
+		return MY_ERROR;
 	}
-	
-	///////////////////////////////////////////////////////////////////////
+
+    if(ioctl(fileDescriptor, I2C_SLAVE, ADDRESS_AS_GND) < 0)
+	{
+		print_error("ioctl() failed. Failed to gain bus access\n");
+		return MY_ERROR;
+	}
 
 	/*
-	* Start the auto sampling mode
-	* by setting the sample rating 
-	* interval.
+	* Configure the AD7992 device.
 	*/
-	if(ad7992Comm.setAutoSampleMode(Tx1024) == MY_ERROR)
+    BYTE bytesToWrite = 2;
+    buff[0] = CONFIGURATION_REG;
+    buff[1] = CONF_REG_VAL_HEX;
+	if(bytesToWrite < 1)
 	{
-		std::cout << "Set auto mode failed" << std::endl;
+		return SUCCESS;
+	}
+	writenByteNum = write(fileDescriptor, buff, bytesToWrite);	
+	if(writenByteNum < 1)
+	{
+		print_error("write failed\n");
+		return MY_ERROR;
 	}
 
 
-	if(ad7992Comm.setAddrRegister(CONVERTION_RESULT_REG) == MY_ERROR)
-	{
-		std::cout << "Setting addres register failed" << std::endl;
-	}	
-	while(1)
-	{
-		if(ad7992Comm.readRegister(buff, 2) == MY_ERROR)
+    WCHAR conVal = 0;
+    BYTE chanel = 0;
+    UINT bytesReaded = 0;
+
+    /*
+    * Save the start time of the sampling.
+    */
+    std::chrono::steady_clock::time_point start = 
+                                       std::chrono::steady_clock::now();
+    while(1)
+    {	
+    	/*
+    	* Ask the device to sample chanel 1 
+    	* and read the sample.
+    	*/
+	    bytesToWrite = 1;
+	    buff[0] = 0x10;
+	    writenByteNum = write(fileDescriptor, buff, bytesToWrite);	
+		if(writenByteNum < 1)
 		{
-			std::cout << "read failed" << std::endl;
+			print_error("write failed\n");
+			return MY_ERROR;
 		}
 
+		bytesReaded = read(fileDescriptor, buff, 2);
+		if(bytesReaded < 1)
+		{
+			print_error("failed to read\n");
+			return MY_ERROR;
+		}
+		conVal = buff[1] + (buff[0] & 0x0f) * 256;
+        chanel = (16 & buff[0]) % 15;   
+        print_debug(conVal, chanel);
 
-		WCHAR conVal = buff[1] + (buff[0] & 0x0f) * 256;
-		BYTE chanel = (16 & buff[0]) % 15;
+        /*
+    	* Ask the device to sample chanel 2
+    	* and read the sample.
+    	*/
+        bytesToWrite = 1;
+	    buff[0] = 0x20;
+	    writenByteNum = write(fileDescriptor, buff, bytesToWrite);	
+		if(writenByteNum < 1)
+		{
+			print_error("write failed\n");
+			return MY_ERROR;
+		}
 
-		printf("Converted value: %d\n",conVal);
-		printf("Ch: %d\n", chanel);
-		delay(500);
-	}
+		bytesReaded = read(fileDescriptor, buff, 2);
+		if(bytesReaded < 1)
+		{
+			print_error("failed to read\n");
+			return MY_ERROR;
+		}
+		conVal = buff[1] + (buff[0] & 0x0f) * 256;
+        chanel = (16 & buff[0]) % 15;
+        print_debug(conVal, chanel);
 
-	return 0;
+        sampleCount++;
+        std::chrono::steady_clock::time_point end = 
+                                              std::chrono::steady_clock::now();
+
+         long microsecsPassed = 
+              std::chrono::duration_cast<std::chrono::microseconds>
+                                                         (end - start).count();
+
+        if(microsecsPassed >= 1000000) 
+        	break;
+        
+    }
+    std::cout << "Took " << sampleCount << " samples from each chanel" 
+                                           << std::endl;
+    return SUCCESS;
 }
+
+
+
+/****************************** End of file ******************************/
